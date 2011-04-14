fa15d12751c179fedb7eb63dde9243595182d287
Yuvaraj Pandian T <yuvipanda@gmail.com>
2011-04-14 18:58:39 +0530

    Slightly better UX for options page. Needs to be better.

---
+++ languages.js
@@ -0,0 +1,12 @@
+primaryIndicLanguages = [
+    {"code": "ta", "name": "தமிழ்", "title": "Tamil"},
+    {"code": "hi", "name": "हिन्दी", "title": "Hindi"},
+    {"code": "ml", "name": "മലയാളം", "title": "Malayalam"},
+];
+
+secondaryIndicLanguages = [
+    {"code": "bpy", "name": "বিষ্ণুপ্রিযা় মণিপুরী", "title": "Bishnupriya Manipuri"},
+    {"code": "mr", "name": "मराठी", "title": "Marathi"},
+    {"code": "te", "name": "తెలుగు", "title": "Telugu"},
+    {"code": "kn", "name": "ಕನ್ನಡ", "title": "Kannada"}    
+];
--- options.js
+++ options.js
@@ -1,6 +1,18 @@
+function appendOption(container, language) {
+    $(container).append("<input type='radio' name='longradio' id='" + language.code + "' /> <label for='" + language.code + "' title='" + language.title + "'>" + language.name + "</label>");
+}
 $(document).ready(function() {
-    $("#lang").val(localStorage["language"]);
-    $("#save").click(function() {
-        localStorage["language"] = $("#lang").val();
+    $.each(primaryIndicLanguages, function(i, v) {
+        appendOption("#primaryIndicChoices", v);
     });
+    $.each(secondaryIndicLanguages, function(i, v) {
+        appendOption("#secondaryIndicChoices", v);
+    });
+    $(".choiceSet").buttonset();
+    var lang = localStorage["language"];
+    $(".choiceSet input[type='radio']").change(function() {
+        lang = $(this).attr('id');
+        localStorage["language"] = lang;
+    });
+    $("#" + lang).attr("checked", "checked").button("refresh");
 });
