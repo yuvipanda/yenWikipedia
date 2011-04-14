8c482dca86fade3a2cf3fe671a05dc79772ceb1c
Yuvaraj Pandian T <yuvipanda@gmail.com>
2011-04-14 23:40:26 +0530

    Works with other languages

--- options.js
+++ options.js
@@ -1,6 +1,14 @@
 function appendOption(container, language) {
     $(container).append("<input type='radio' name='longradio' id='" + language.code + "' value='" + language.name + "' /> <label for='" + language.code + "' title='" + language.title + "'>" + language.name + "</label>");
 }
+function saveLang(lang, name) {
+        localStorage["language"] = lang;
+        $("#notice").clearQueue();
+        $("#notice").text("Language changed to " + name + "");
+
+        $("#notice").fadeIn().delay(3000).fadeOut();
+
+}
 $(document).ready(function() {
     $.each(primaryIndicLanguages, function(i, v) {
         appendOption("#primaryIndicChoices", v);
@@ -9,27 +17,26 @@ $(document).ready(function() {
         appendOption("#secondaryIndicChoices", v);
     });
     $(".choiceSet").buttonset();
+    $("#showOther").button();
+    $("#saveOther").button();
     var lang = localStorage["language"];
     $(".choiceSet input[type='radio']").change(function() {
-        lang = $(this).attr('id');
-        localStorage["language"] = lang;
-        $("#notice").clearQueue();
-        $("#notice").text("Language changed to " + $(this).attr('value') + "");
-
-        $("#notice").fadeIn().delay(3000).fadeOut();
+        saveLang($(this).attr("id"), $(this).attr("value"));
+    });
+    $("#otherChoice").val(lang);
+    $("#saveOther").click(function() {
+        saveLang($("#otherChoice").val(), $("#otherChoice").val());
     });
     $("#" + lang).attr("checked", "checked").button("refresh");
     $("label").removeClass("ui-corner-left").removeClass("ui-corner-right");
 
     $("#showOther").click(function() {
         if ($("#showOther").text() == "Hide") {
-            $("#otherEntry").fadeOut(function() {
-                $("#showOther").text("Other Language?");
-            });
+            $("#otherEntry").fadeOut();
+            $("#showOther").html("Other&nbsp;Language?");
         } else {
-            $("#otherEntry").fadeIn(function() {
-            $("#showOther").text("Hide");
-            });
+            $("#otherEntry").fadeIn();
+            $("#showOther").html("Hide");
         }
     });
     $("#container").fadeIn();
