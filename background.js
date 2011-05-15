813e24c8c5d2fcdad9f6f22eaf798868b39506fd
Yuvaraj Pandian T <yuvipanda@gmail.com>
2011-05-15 08:09:43 +0530

    Added caching of results. Expiry time 30 days

--- background.js
+++ background.js
@@ -1,17 +1,35 @@
 var defaultLanguage = 'ta';
+var expiryTime = 31 * 60 * 60 * 24; // 31 Days expiry time
+
+function doFoundCallback(object, lang, callback) {
+    callback({'title': object[lang],'lang': lang});
+}
+
+function objectifyLinks(data, callback) {
+    object = {};
+    $(data).find("ll").each(function() {
+        object[$(this).attr("lang")] = $(this).text();
+        callback(object);
+    });
+}
 
 function getLocalName(title, callback) {
-    var url = "http://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=langlinks&lllimit=500&format=xml";
     var lang = localStorage['language'];
     if (!lang) {
         lang = 'ta';
     }
-    $.get(url, dataType="xml", function(data) {
-        $(data).find("ll[lang='" + lang + "']").each(function() {
-            callback({'title': $(this).text(),'lang': lang});
+    if (lscache.get(title)) {
+        object = lscache.get(title);
+        doFoundCallback(object, lang, callback);
+    } else {
+        var url = "http://en.wikipedia.org/w/api.php?action=query&titles=" + title + "&prop=langlinks&lllimit=500&format=xml";
+        $.get(url, dataType="xml", function(data) {
+            objectifyLinks(data, function(object) { 
+                lscache.set(title, object, expiryTime); 
+                doFoundCallback(object, lang, callback);
+            });
         });
-    });
-
+    }
 }
 
 function onRequest(request, sender, callback) {
